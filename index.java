1d89273937b26292003d149af7a1e8ac5b02440d
Anjali RAidas <anjaliraidas1@gmail.com>
2025-07-02 11:07:58 +0530

    initial commit

---
+++ index.java
@@ -0,0 +1,16 @@
+
+
+public class index {
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 7};
+        int max = arr[0];
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        System.out.println(max);
+    }
+}
