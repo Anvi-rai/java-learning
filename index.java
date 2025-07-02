f331e538259ddeff039110b2e5a544802c8d998b
Anjali RAidas <anjaliraidas1@gmail.com>
2025-07-02 11:44:50 +0530

    array

--- index.java
+++ index.java
@@ -1,16 +1,32 @@
 
+// public class index {
+//     public static void main(String[] args) {
+//         int[] arr = {1, 2, 3, 4, 5, 7};
+//         int max = arr[0];
+//         for (int i = 0; i < arr.length; i++) {
+//             if (arr[i] > max) {
+//                 max = arr[i];
+//             }
+//         }
+//         System.out.println(max);
+//     }
+// }
+import java.util.Scanner;
 
-public class index {
+class index {
 
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 4, 5, 7};
-        int max = arr[0];
-
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] > max) {
-                max = arr[i];
-            }
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the size of the array:");
+        int n = in.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = in.nextInt();
         }
-        System.out.println(max);
+        for (int k = 0; k < arr.length; k++) {
+            System.out.println(arr[k] + "  " + "elem");
+        }
+
     }
+
 }
