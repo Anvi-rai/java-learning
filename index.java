b65d23ea2ab5cf9f540ff80e4b66ac720eb71ece
Anjali RAidas <anjaliraidas1@gmail.com>
2025-07-03 11:01:21 +0530

    comman element

--- index.java
+++ index.java
@@ -53,18 +53,37 @@ class index {
     public static void main(String[] args) {
         int[] arr1 = {1, 2, 3, 4, 5,};
         int[] arr2 = {4, 2, 4, 2, 4};
-        int[] marge = new int[arr1.length + arr2.length];
         for (int i = 0; i < arr1.length; i++) {
-            marge[i] = arr1[i];
+            for (int j = 0; j < arr2.length; j++) {
+                if (arr1[i] == arr2[j]) {
+                    System.out.println("Common element: " + arr1[i]);
+                    break;
+                }
+            }
         }
-        for (int i = 0; i < arr2.length; i++) {
-            marge[arr1.length + i] = arr2[i];
-        }
-        for (int i = 0; i < marge.length; i++) {
-            System.out.println(marge[i]);
-        }
-        //         int sum = 0
-        ;
+
+    }
+} // int max = arr1[0] + arr2[0];
+// for (int i = 0; i < arr1.length; i++) {
+//     for (int j = 0; j < arr2.length; j++) {
+//         if (arr1[i] + arr2[j] > max) {
+//             max = arr1[i] + arr2[j];
+//         }
+//     }
+// }
+// System.out.println(max);
+// int[] marge = new int[arr1.length + arr2.length];
+// for (int i = 0; i < arr1.length; i++) {
+//     marge[i] = arr1[i];
+// }
+// for (int i = 0; i < arr2.length; i++) {
+//     marge[arr1.length + i] = arr2[i];
+// }
+// for (int i = 0; i < marge.length; i++) {
+//     System.out.println(marge[i]);
+// }
+//         int sum = 0
+;
 //         for (int i = 0; i < arr1.length; i++) {
 //             sum = sum + arr1[i] + arr2[i];
 
@@ -76,5 +95,4 @@ class index {
 //             // }
 //         }
 //         System.out.println(sum);
-    }
-}
+
