33b95d8257fa7cf1010a9a7910ce59028ed638e6
Anjali RAidas <anjaliraidas1@gmail.com>
2025-07-03 13:08:19 +0530

    sum 2d array

--- index.java
+++ index.java
@@ -126,11 +126,18 @@ class index {
 
     public static void main(String[] args) {
         int[][] arr = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        // for (int i = 0; i < arr.length; i++) {
+        //     for (int j = 0; j < arr[i].length; j++) {
+        //         System.out.print(arr[i][j] + " ");
+        //     }
+        //    System.out.println();
+        // }
+        int sum = 0;
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr[i].length; j++) {
-                System.out.print(arr[i][j] + " ");
+                sum += arr[i][j];
             }
-            System.out.println();
+            System.out.println("Sum of row " + i + ": " + sum);
         }
     }
 }
