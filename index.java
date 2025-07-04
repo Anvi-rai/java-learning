678ca55a8303fec7a82ce406b70bc20f8cf264db
Anjali RAidas <anjaliraidas1@gmail.com>
2025-07-04 23:46:22 +0530

    copy array

--- index.java
+++ index.java
@@ -122,22 +122,36 @@
 //         }
 //     }
 // }
+// class index {
+//     public static void main(String[] args) {
+//         int[][] arr = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+// for (int i = 0; i < arr.length; i++) {
+//     for (int j = 0; j < arr[i].length; j++) {
+//         System.out.print(arr[i][j] + " ");
+//     }
+//    System.out.println();
+// }
+//         int sum = 0;
+//         for (int i = 0; i < arr.length; i++) {
+//             for (int j = 0; j < arr[i].length; j++) {
+//                 sum += arr[i][j];
+//             }
+//             System.out.println("Sum of row " + i + ": " + sum);
+//         }
+//     }
+// }
 class index {
 
     public static void main(String[] args) {
-        int[][] arr = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-        // for (int i = 0; i < arr.length; i++) {
-        //     for (int j = 0; j < arr[i].length; j++) {
-        //         System.out.print(arr[i][j] + " ");
-        //     }
-        //    System.out.println();
-        // }
-        int sum = 0;
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr[i].length; j++) {
-                sum += arr[i][j];
-            }
-            System.out.println("Sum of row " + i + ": " + sum);
+        int[] arr1 = {1, 2, 3, 4, 5};
+        int[] arr2 = new int[arr1.length];
+
+        for (int i = 0; i < arr1.length; i++) {
+            arr2[i] = arr1[i];
+        }
+        System.out.print("Copied array: ");
+        for (int value : arr2) {
+            System.out.print(value + " ");
         }
     }
 }
